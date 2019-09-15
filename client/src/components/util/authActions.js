d6c22ece9e6bba18572e3a4924f64d741082d6a4
Kaemon Lovendahl <k-manskrazy@hotmail.com>
2019-09-15 10:28:11 -0600

    Added a lot of console logs

--- client/src/components/util/authActions.js
+++ client/src/components/util/authActions.js
@@ -4,21 +4,28 @@ import { authRequest, authSuccess, authFailure, logout } from '../../reducers/au
 export const registerUser = (dispatch, user) => {
   axios.post('/api/v1/users', { user: {...user}})
     .then(response => {
+      console.log("Create User: ", response)
       authenticate(dispatch, response.data)
     })
-    .catch(errors => console.log(errors))
+    .then(response => console.log("After User Createion: ", response))
+    .catch(errors => console.log("Errors Registering: ", errors))
 }
 
 export const authenticate = (dispatch, user) => {
+  console.log("Authentication User Param: ", user)
   dispatch(authRequest())
   axios.post('/authenticate', user)
     .then(response => {
+      console.log("Auth Response: ", response)
       let authToken = response.data.auth_token
       localStorage.setItem('JWT', `${authToken}`)
+      console.log("AuthToken: ", authToken)
       dispatch(authSuccess(user, authToken))
     })
     .catch(errors => {
+      console.log("Auth Failed: ", errors)
       dispatch(authFailure(errors))
+      localStorage.clear()
     })
 }
 
