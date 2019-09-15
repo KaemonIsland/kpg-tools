b4f8d8d411945ef4b4238d93f716b15bebe2811c
Kaemon Lovendahl <k-manskrazy@hotmail.com>
2019-09-15 08:16:25 -0600

    Updated CORS File

--- config/application.rb
+++ config/application.rb
@@ -26,14 +26,6 @@ module KpgTools
 
     config.autoload_paths << Rails.root.join('lib')
 
-    # For CORS
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
-      end
-    end
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
